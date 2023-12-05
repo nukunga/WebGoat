ba1011a26ce07900310f6648d02f35b9d312b3ce
nukunga <145279280+nukunga@users.noreply.github.com>
2023-12-06 02:27:09 +0900

    #

--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
-cd /home/ubuntu/server/target/
-java -jar webgoat-2023.6-SNAPSHOT.jar --server.port=8080 --server.address=52.78.138.74 
+#cd /home/ubuntu/server/target/
+#java -jar webgoat-2023.6-SNAPSHOT.jar --server.port=8080 --server.address=52.78.138.74 
+
+
+BUILD_JAR=$(ls /home/ubuntu/server/target/*.jar)
+JAR_NAME=$(basename "$BUILD_JAR")
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
+
+echo "> build 파일 복사" >> /home/ubuntu/deploy.log
+DEPLOY_PATH=/home/ubuntu/
+cp "$BUILD_JAR" $DEPLOY_PATH
+
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
+CURRENT_PID=$(pgrep -f "$JAR_NAME")
+
+if [ -z "$CURRENT_PID" ]
+then
+  echo "> noting to kill" >> /home/ubuntu/deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 "$CURRENT_PID"
+  sleep 5
+fi
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> MONGO_URL:"
+echo "$MONGO_URL"
+echo "> run DEPLOY_JAR"    >> /home/ubuntu/deploy.log
+
+nohup java -jar "$DEPLOY_JAR" --server.port=8080 --server.address=52.78.138.74
